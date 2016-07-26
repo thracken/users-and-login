3d3605174b1265a6a4109fd3db837de2ca1f02be
Jeff <jeffgreenland.1@gmail.com>
2016-07-26 12:04:24 -0700

    Added ability to log in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,17 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_user_in user
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Invalid login info. Please try again."
+      render 'new'
+    end
+  end
+
   def destroy
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+  def log_user_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @curerent_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
